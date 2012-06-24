1ebdb20a40535ada75aab33a5bc52e8de844eadf
kirillyar <kirillyar@gmail.com>
2012-06-25 02:03:56 +0600

    Добавил модели, необходимо базу завести и syncdb сделать

--- general/models.py
+++ general/models.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+
+class ProductType:
+    name = models.CharField(max_length=200)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField()
+    type = models.ForeignKey(ProductType)
+    parameters = models.ManyToManyField(ProductParameter, through=ProductParameterValue)
+
+class ProductParameter:
+    name = models.CharField(max_length=200)
+
+class ProductParameterValue:
+    parameter = models.ForeignKey(ProductParameter)
+    product = models.ForeignKey(Product)
+    Value = models.CharField(max_length=200)
