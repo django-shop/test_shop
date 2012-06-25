9e4ad91eb1696e1d8835ec9631b63123e353ac16
kirillyar <kirillyar@gmail.com>
2012-06-26 01:08:00 +0600

    Поправил модели, должно быть все ок

--- general/models.py
+++ general/models.py
@@ -4,16 +4,21 @@ from django.db import models
 class ProductType:
     name = models.CharField(max_length=200)
 
+class ProductParameter:
+    name = models.CharField(max_length=200)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField()
     type = models.ForeignKey(ProductType)
-    parameters = models.ManyToManyField(ProductParameter, through=ProductParameterValue)
-
-class ProductParameter:
-    name = models.CharField(max_length=200)
+    parameters = models.ManyToManyField(ProductParameter, through='ProductParameterValue')
 
 class ProductParameterValue:
     parameter = models.ForeignKey(ProductParameter)
     product = models.ForeignKey(Product)
     Value = models.CharField(max_length=200)
+
+
+
+
